91d6431a6aec84c777fc1ec26d17a8aef7b4f494
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-16 13:27:13 +0200

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Eric on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
