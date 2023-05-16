aad5c4d451685d0a9f87537dd70bc49637cab355
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-16 14:12:23 +0200

    - Add save method to contactListViewModel

--- ContactList/ViewModels/ContactListViewModel.swift
+++ ContactList/ViewModels/ContactListViewModel.swift
@@ -13,4 +13,16 @@ class ContactListViewModel: ObservableObject {
     init() {
         contacts = [Person(name: "Eric", company: "Apple Corporation"), Person(name: "Stephan", company: "Microsoft TM")]
     }
+    
+    func savePerson(person: Person) {
+        if person.id == nil {
+            var newPerson = person
+            newPerson.id = UUID().uuidString
+            contacts.append(newPerson)
+        } else {
+            if let index = contacts.firstIndex(where: { $0.id == person.id }) {
+                contacts[index] = person
+            }
+        }
+    }
 }
